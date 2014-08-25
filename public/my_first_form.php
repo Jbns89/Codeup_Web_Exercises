89bcec882e4aedb422e94cc45e85e76464fdf88c
Jbns89 <Jbns89@gmail.com>
2014-08-25 13:01:03 -0500

    email body

--- public/my_first_form.php
+++ public/my_first_form.php
@@ -8,22 +8,50 @@ var_dump($_GET);
 </head>
 
 <body>
-	<form method="GET">
-    <p>
-        <label for="username">Username</label>
-        <input id="username" name="username" type="text">
-    </p>
-    <p>
-        <label for="password">Password</label>
-        <input id="password" name="password" type="password">
-    </p>
-    <p>
-        <input type="submit">
-    </p>
-</form>
+	<h2>User Login</h2>
+		<form method="POST">
+    		<p>
+        		<label for="username">Username</label>
+        		<input id="username" name="username" type="text	" placeholder="Enter your username">
+    			<!-- the key value is taken from the name 	attribute -->
+    		</p>
+    		<p>
+        		<label for="password">Password</label>
+        		<input id="password" name="password" type="	password" placeholder="Enter your password">
+        		<!-- id type password - you can't see text
+        		id type text - you can see text
+        		id type comments - lets you create a text box -->
+    		</p>
+   			<p>
+        		<button type="submit">Login</button>
+    		</p>
+		</form>
+	<h2>Compose an Email<h2>
+		<form method="POST">
+			<p>
+				<label for="To:">To:</label>
+        		<input id="To:" name="To:" type="text	" placeholder="Enter email">
+			</p>
+			<p>
+				<label for="From:">From:</label>
+        		<input id="From:" name="From:" type="text	" placeholder="Enter email">
+			</p>
+			<p>
+				<label for="Subject">Subject</label>
+        		<input id="Subject" name="Subject" type="text	" placeholder="Please enter subject">
+			</p>
+			<p>
+				<label for="Body">Body</label>
+        		<textarea id="Body" name="Body" rows="5" cols="40">Enter Message Here</textarea>
+			</p>
+			<p>
+        		<button type="submit">Send</button>
+    		</p>
+		</form>
 </body>
 
 </html>
 
 
-<!--  --> <!--  -->
+
+
